35ec9c188bc063ec9af7437e5deb6acbf5b19856
Vladimir Semenov <subatiq@gmail.com>
2024-07-18 15:15:57 +0300

    Remove stdout

--- status.sh
+++ status.sh
@@ -1,3 +1,3 @@
 (nc -w0 $sut_addr 22 2> /dev/null && echo "ssh_state=up") || echo "ssh_state=down"
 curl "http://$plug_ip/relay/0" 2> /dev/null  | jq '.ison' | xargs -I {} echo ison={}
-(ping -c1 -W1 $sut_addr 2> /dev/null && echo "ping=true") || echo "ping=false"
+(ping -c1 -W1 $sut_addr > /dev/null && echo "ping=true") || echo "ping=false"
