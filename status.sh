3611274a5c82dc05afa98c8253e7ff1628d4242b
Vladimir Semenov <subatiq@gmail.com>
2024-07-18 15:59:34 +0300

    Fix nc timeout

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
-(nc -w0.5 $sut_addr 22 2> /dev/null && echo "ssh_state=up") || echo "ssh_state=down"
+(nc -w1 $sut_addr 22 &> /dev/null && echo "ssh_state=up") || echo "ssh_state=down"
 curl "http://$plug_ip/relay/0" 2> /dev/null  | jq '.ison' | xargs -I {} echo ison={}
 (ping -c1 -W1 $sut_addr > /dev/null && echo "ping=true") || echo "ping=false"
 echo "target=$target_alias"
